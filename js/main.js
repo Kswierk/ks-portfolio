242c683b8e22313736e826c577263ec6d38f2111
Kswierk <kacper.swierk@gmail.com>
2020-07-21 14:24:22 +0200

    side-navbar added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navbar__list');
+    const navLinks = document.querySelectorAll('.navbar__item')
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('navbar__list--active');
+
+        // navbar slide
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navlinkFade 0.5s ease forwards ${index / 7}s`
+            }
+        });
+
+        // burger animation
+        burger.classList.toggle('toggle')
+    });
+
+};
+
+navSlide();
